0074713dd368b86f12aace812b7f24f3640ed901
shrad4567 <shraddha.agrawal4567@gmail.com>
2026-03-26 11:56:55 +0530

    Update streamlit_app.py
    asd

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if ingredient_list:
 
 
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response
+st.text(smoothiefroot_response);
